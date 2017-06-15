ff1947e69eba77b8e6d5c8a514c6a396cc4c2f4b
David Wong <deadwater88@gmail.com>
2017-06-15 01:57:17 -0700

    implement kth greatest

--- david_wong/lib/k_largest_elements.rb
+++ david_wong/lib/k_largest_elements.rb
@@ -2,19 +2,13 @@ require_relative 'heap'
 
 def k_largest_elements(array, k)
   output = []
-  heap = BinaryMinHeap.new {|a,b| b <=> a}
-  (array.length - k).times { heap.push(array.pop) }
-  min = [k, heap.count].min
-  while output.length < k
-    p array
-    p output
-    p heap.store
-    debugger
-    if array.last >= heap.peek
-      output.push(array.pop)
-    else
-      output.push(heap.extract(min))
+  heap = BinaryMinHeap.new {|a,b| a <=> b}
+  k.times { heap.push(array.pop) }
+  array.each do |num|
+    if num >= heap.peek
+      heap.store.push(num)
+      heap.extract
     end
   end
-  output
+  heap.store
 end
