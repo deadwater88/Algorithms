8a3dc0a501e0729a4e4c3a5be2b81273fb2bf050
David Wong <deadwater88@gmail.com>
2017-06-20 01:45:02 -0700

    shortened bst practical

--- binarysearch/david_wong/lib/bst_practical.rb
+++ binarysearch/david_wong/lib/bst_practical.rb
@@ -4,8 +4,7 @@ require_relative './binary_search_tree.rb'
 def kth_largest(tree_node, k, node = {correct: nil, count: 0})
   return if tree_node.nil? || node[:correct]
   kth_largest(tree_node.right, k, node)
-  node[:count] += 1
-  node[:correct] = tree_node if k == node[:count]
+  node[:correct] = tree_node if k == (node[:count] += 1)
   kth_largest(tree_node.left, k, node)
   node[:correct]
 end
